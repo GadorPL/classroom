e014a6c6629bade6eb9a6ce02f4599221b822905
Michał Hewelt <mhewelt9@gmail.com>
2022-12-15 11:00:06 +0100

    add teacher model

---
+++ classroom/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import home_view
+
+app_name = 'classroom'
+# domain.com/classroom/
+
+urlpatterns = [
+    path('', home_view, name='home') #path expects a function
+]
+
