88ba7351b27802d3d8f12e8abd5e35396a63355c
Michał Hewelt <mhewelt9@gmail.com>
2022-12-15 15:00:20 +0100

    use FormView

--- classroom/urls.py
+++ classroom/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomeView, ThankYouView
+from .views import HomeView, ThankYouView, ContactFormView
 
 app_name = 'classroom'
 # domain.com/classroom/
@@ -7,5 +7,6 @@ app_name = 'classroom'
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),  # path expects a function
     path('thank_you/', ThankYouView.as_view(), name='thank_you'),
+    path('contact/', ContactFormView.as_view(), name='contact'),
 ]
 
--- classroom/views.py
+++ classroom/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView
+from classroom.forms import ContactForm
+from django.urls import reverse, reverse_lazy
 
 
 class HomeView(TemplateView):
@@ -8,3 +10,17 @@ class HomeView(TemplateView):
 
 class ThankYouView(TemplateView):
     template_name = 'classroom/thank_you.html'
+
+
+class ContactFormView(FormView):
+    form_class = ContactForm
+    template_name = 'classroom/contact.html'
+
+    # URL NOT a template you want to connect to
+    success_url = "/classroom/thank_you/"
+
+    # what to do with form?
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        # ContactForm(request.POST)
+        return super().form_valid(form)
